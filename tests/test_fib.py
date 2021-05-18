e1d0b6e2dc674f471defe77dc1980ecbe3c9303e
Charles M <info@charlem.com.ng>
2021-05-18 08:25:26 +0000

    Updating Tests

--- tests/test_fib.py
+++ tests/test_fib.py
@@ -1,9 +1,9 @@
 from fib import fibnumber, fibseries
 
-'''
+
 def test_fibseries():
-    assert fibseries.fibseries(0) == '0'
-'''
+    assert fibseries.fibseries(0) == None
+
 
 def test_fibnumber():
     assert fibnumber.fibnumber(20) == 6765
